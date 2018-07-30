8212acc54c2ed19ecadf1cab85decf2c8ecfee2b
SteffenEriksen <SteffenEriksen@users.noreply.github.com>
2018-07-30 10:37:59 +0200

    Update StringCompareExamples.cs

--- C#/StringCompareExamples.cs
+++ C#/StringCompareExamples.cs
@@ -1,1 +1,10 @@
 
+// Example for dictionary
+var lookup = new Dictionary<string, object>(StringComparer.InvariantCultureIgnoreCase);
+
+// Example for List
+var list = new List<string> { "tESt" };
+Console.WriteLine(list.Contains("test", StringComparer.InvariantCultureIgnoreCase));
+
+// Example for string
+Console.WriteLine("tEst".Equals("test", StringComparison.InvariantCultureIgnoreCase));
